8a207a71d37d6331dd3a4c49fdfafcb1b9ba6cff
Ilia Urvachev <rtm@ctrlz.ru>
2015-02-17 14:21:34 +0300

    Initial Commit

---
+++ ParrotZik/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ParrotZik
+//
+//  Created by Внештатный командир земли on 17/02/15.
+//  Copyright (c) 2015 Внештатный командир земли. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
