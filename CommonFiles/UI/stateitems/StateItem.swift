d0b947e38f6645d0a3b7137ae56bedd1ee823063
Ilia Urvachev <rtm@ctrlz.ru>
2015-02-18 17:13:05 +0300

    added headers name to menu

---
+++ CommonFiles/UI/stateitems/StateItem.swift
@@ -0,0 +1,9 @@
+//
+//  StateItem.swift
+//  ParrotZik
+//
+//  Created by Внештатный командир земли on 18/02/15.
+//  Copyright (c) 2015 Внештатный командир земли. All rights reserved.
+//
+
+import Foundation
